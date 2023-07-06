2d7d26cd0424004ebf17803e581e516f7f523d0e
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-05 20:04:01 -0400

    add pizza component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Pizza } from "./components";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello React!</h1>
+      <Pizza />
     </>
   );
 }
---
+++ src/components/Pizza.tsx
@@ -0,0 +1,11 @@
+import pizzaSpinaci from "../assets/spinaci.jpg";
+
+const Pizza = () => {
+  return (
+    <div>
+      <img src={pizzaSpinaci} alt="Pizza spinaci" />
+    </div>
+  );
+};
+
+export default Pizza;
